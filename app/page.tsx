b78c22d8120572439b8d526d04b140776156f5c4
liaoxingfeng <liaoxingfeng@iyinguo.com>
2026-01-12 18:08:36 +0800

    feat: 添加.gitignore文件

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+/*
+ * @Description: Description
+ * @Author: liaoxingfeng@iyinguo.com
+ */
 import Image from "next/image";
 
 export default function Home() {
@@ -14,10 +18,11 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            Opps! Welcome to yg zone! Let's get started.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            {/* Looking for a starting point or more instructions? Head over to{" "} */}
+            这是一个用于活动榜单数据创建的项目
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
